252ecb95e72c6192216ce688e65c8e1b47ee8280
MarioRuiz <MarioRuizS@gmail.com>
2021-07-30 11:38:39 +0000

    get channel members limited to 1000

--- lib/slack/smart-bot/comm/get_channel_members.rb
+++ lib/slack/smart-bot/comm/get_channel_members.rb
@@ -1,9 +1,10 @@
+#todo: add pagination for case more than 1000 members in the channel
 def get_channel_members(channel_id)
     begin
         if config.simulate and config.key?(:client)
             client.web_client.conversations_members[channel_id.to_sym].members
         else
-            client.web_client.conversations_members(channel: channel_id).members
+            client.web_client.conversations_members(channel: channel_id, limit: 1000).members
         end
     rescue Exception => stack
         @logger.warn stack

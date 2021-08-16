1ffec66e7031af1fc588906fb1329312ca4f42a4
MarioRuiz <MarioRuizS@gmail.com>
2021-08-16 11:54:15 +0000

    get_channels next_cursor

--- lib/slack/smart-bot/comm/get_channels.rb
+++ lib/slack/smart-bot/comm/get_channels.rb
@@ -10,21 +10,23 @@ def get_channels(bot_is_in: false)
       if bot_is_in
         client.web_client.users_conversations(exclude_archived: true, limit: 1000, types: "im, public_channel,private_channel").channels
       else
-        resp = {}
-        next_cursor = ""
-        begin
-          resp.merge!(client.web_client.conversations_list(
-            cursor: next_cursor,
-            types: "private_channel, public_channel",
+        resp = client.web_client.conversations_list(types: "private_channel,public_channel", limit: "1000", exclude_archived: "true")
+        channels = resp.channels
+        while resp.response_metadata.next_cursor.to_s != ""
+          resp = client.web_client.conversations_list(
+            cursor: resp.response_metadata.next_cursor.to_s,
+            types: "private_channel,public_channel",
             limit: "1000",
             exclude_archived: "true",
-          ))
-          next_cursor = resp.response_metadata.next_cursor.to_s
-        end until next_cursor == ""
-        return resp.channels
+          )
+          channels += resp.channels
+        end
+
+        return channels
       end
     end
   rescue Exception => stack
     @logger.warn stack
+    return []
   end
 end

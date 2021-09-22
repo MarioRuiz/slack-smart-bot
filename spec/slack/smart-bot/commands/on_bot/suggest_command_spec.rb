288af888c54df4385bcaec56b04d92225b634d7e
MarioRuiz <MarioRuizS@gmail.com>
2021-09-22 09:35:29 +0000

    fix #30

--- spec/slack/smart-bot/commands/on_bot/suggest_command_spec.rb
+++ spec/slack/smart-bot/commands/on_bot/suggest_command_spec.rb
@@ -7,21 +7,24 @@ RSpec.describe SlackSmartBot, "suggest_command" do
       it "responds" do
         send_message "suggest command", from: :uadmin, to: channel
         expect(buffer(to: channel, from: :ubot).join).to match(/Command suggestion/i)
-        expect(bufferc(to: channel, from: :ubot).join).not_to match(/echo SOMETHING|run something|which rules|go to sleep/i)
+        expect(buffer(to: channel, from: :ubot).join).to match(/`.+`/i)
         send_message "random command", from: :uadmin, to: channel
         expect(buffer(to: channel, from: :ubot).join).to match(/Command suggestion/i)
-        expect(bufferc(to: channel, from: :ubot).join).not_to match(/echo SOMETHING|run something|which rules|go to sleep/i)
+        expect(buffer(to: channel, from: :ubot).join).to match(/`.+`/i)
         send_message "command suggestion", from: :uadmin, to: channel
         expect(buffer(to: channel, from: :ubot).join).to match(/Command suggestion/i)
-        expect(bufferc(to: channel, from: :ubot).join).not_to match(/echo SOMETHING|run something|which rules|go to sleep/i)
+        expect(buffer(to: channel, from: :ubot).join).to match(/`.+`/i)
         send_message "suggest rule", from: :uadmin, to: channel
         expect(buffer(to: channel, from: :ubot).join).to match(/Command suggestion/i)
+        expect(buffer(to: channel, from: :ubot).join).to match(/`.+`/i)
         expect(bufferc(to: channel, from: :ubot).join).to match(/echo SOMETHING|run something|which rules|go to sleep/i)
         send_message "random rule", from: :uadmin, to: channel
         expect(buffer(to: channel, from: :ubot).join).to match(/Command suggestion/i)
+        expect(buffer(to: channel, from: :ubot).join).to match(/`.+`/i)
         expect(bufferc(to: channel, from: :ubot).join).to match(/echo SOMETHING|run something|which rules|go to sleep/i)
         send_message "rule suggestion", from: :uadmin, to: channel
         expect(buffer(to: channel, from: :ubot).join).to match(/Command suggestion/i)
+        expect(buffer(to: channel, from: :ubot).join).to match(/`.+`/i)
         expect(bufferc(to: channel, from: :ubot).join).to match(/echo SOMETHING|run something|which rules|go to sleep/i)
       end
     end

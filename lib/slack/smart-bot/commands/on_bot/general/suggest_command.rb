ab1069dd5023d51fc4197f8ab27edfec0cabaf35
MarioRuiz <MarioRuizS@gmail.com>
2022-04-05 10:34:42 +0000

    suggest command added dont_suggest

--- lib/slack/smart-bot/commands/on_bot/general/suggest_command.rb
+++ lib/slack/smart-bot/commands/on_bot/general/suggest_command.rb
@@ -14,9 +14,13 @@ class SlackSmartBot
   # help:
   def suggest_command(from, dest, dchannel, specific, rules_file)
     save_stats(__method__)
+    dont_suggest = []
     help_message = get_help(rules_file, dest, from, specific, true, descriptions: false, only_normal_user: true)
     commands = help_message.gsub(/====+/,'-'*30).split(/^\s*-------*$/).flatten
     commands.reject!{|c| c.match?(/These are specific commands for this bot on this/i) || c.match?(/\A\s*\z/)}
+    dont_suggest.each do |ds|
+      commands.reject!{|c| c.match?(/:#{ds}\s*$/i)}
+    end
     @last_suggested_commands ||= []
     @last_suggested_commands.shift if @last_suggested_commands.size >=5
     command = ''

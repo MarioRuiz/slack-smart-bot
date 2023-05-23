deb7a4200bc3c94e2843bd0d178a44cc1c7e585a
MarioRuiz <MarioRuizS@gmail.com>
2023-05-23 11:55:45 +0000

    unreact admits cahnnel param

--- lib/slack/smart-bot/comm/unreact.rb
+++ lib/slack/smart-bot/comm/unreact.rb
@@ -2,7 +2,7 @@ class SlackSmartBot
   # list of available emojis: https://www.webfx.com/tools/emoji-cheat-sheet/
   # unreact(:thumbsup)
   # ts: can be true, false or a specific ts
-  def unreact(emoji, ts=false)
+  def unreact(emoji, ts=false, channel: Thread.current[:dest])
     begin
       if ts.is_a?(TrueClass) or ts.is_a?(FalseClass)
         parent = ts
@@ -21,7 +21,7 @@ class SlackSmartBot
         @logger.warn 'unreact method no ts supplied'
       else
         begin
-          client.web_client.reactions_remove(channel: Thread.current[:dest], name: emoji, timestamp: ts) unless config.simulate
+          client.web_client.reactions_remove(channel: channel, name: emoji, timestamp: ts) unless config.simulate
         rescue Exception => stack
           @logger.warn stack
         end
